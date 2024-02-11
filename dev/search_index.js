314397a3d4597442a2b8739b3fd13f3cff67e230
Documenter.jl <documenter@juliadocs.github.io>
2024-02-11 19:03:58 +0000

    build based on 921619d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LabArduinoShutter","category":"page"},{"location":"#LabArduinoShutter","page":"Home","title":"LabArduinoShutter","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LabArduinoShutter.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LabArduinoShutter]","category":"page"}]
+}
